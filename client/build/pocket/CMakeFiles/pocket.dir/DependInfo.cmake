71a61c3299d0919454cc0ab63b9466db3e46121d
adamqqqplay <adamqqq@163.com>
2021-03-03 08:15:03 +0000

    Complier client library

---
+++ client/build/pocket/CMakeFiles/pocket.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/project/pocket/client/pocket/pocket_dispatcher.cc" "/home/ubuntu/project/pocket/client/build/pocket/CMakeFiles/pocket.dir/pocket_dispatcher.cc.o"
+  "/home/ubuntu/project/pocket/client/pocket/pypocket.cxx" "/home/ubuntu/project/pocket/client/build/pocket/CMakeFiles/pocket.dir/pypocket.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_PYTHON_DYN_LINK"
+  "pocket_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../client"
+  "../pocket"
+  "pocket"
+  "../"
+  "/usr/include/python3.5"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/project/pocket/client/build/client/CMakeFiles/cppcrail.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
